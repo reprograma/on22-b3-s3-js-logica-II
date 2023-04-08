b142290ac3484c7d754e114692990508842764e1
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-04-08 12:01:19 -0300

    commits

---
+++ material/test_function_return.js
@@ -0,0 +1,11 @@
+function retornaValor(){
+    let valor = "valor do valor"
+
+    return valor
+}
+
+let valorRetornado = retornaValor()
+
+for (i=0; i<10; i++){
+    console.log(valorRetornado)  
+}
