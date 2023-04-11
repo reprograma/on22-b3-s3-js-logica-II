de75cbcb62242dd947e93d692db769e08ed28305
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-04-11 12:42:17 -0300

    resolvendo exercicio2 com switch case

---
+++ exercicios/para-casa/script2.js
@@ -0,0 +1,55 @@
+//2. Crie uma função que receba um número de mês como parâmetro e retorne o nome do mês correspondente. Use a estrutura switch case para resolver este problema.//
+
+function convertendoStrEmInt(mes) {
+    switch (mes) {
+        case 1:
+            console.log("Janeiro")
+            break
+        case 2:
+            console.log("Fevereiro")
+            break    
+        case 3:
+            console.log("Março")
+            break
+        case 4:
+            console.log("Abril")
+            break
+        case 5:
+            console.log("Maio") 
+            break
+        case 6:
+            console.log("Junho")
+            break    
+        case 7:
+            console.log("Julho")
+            break
+        case 8:
+            console.log("Agosto")
+            break
+        case 9:
+            console.log("Setembro")
+            break
+        case 10:
+            console.log("Outubro")
+            break
+        case 11:
+            console.log("Novembro")
+            break
+        case 12:
+            console.log("Dezembro")
+            break
+    }
+    return mes
+}
+mesUm = convertendoStrEmInt(1)
+mesDois = convertendoStrEmInt(2)
+mesTres = convertendoStrEmInt(3)
+mesQuatro = convertendoStrEmInt(4)
+mesCinco = convertendoStrEmInt(5)
+mesSeis = convertendoStrEmInt(6)
+mesSete = convertendoStrEmInt(7)
+mesOito = convertendoStrEmInt(8)
+mesNove = convertendoStrEmInt(9)
+mesDez = convertendoStrEmInt(10)
+mesOnze = convertendoStrEmInt(11)
+mesDoze = convertendoStrEmInt(12)
