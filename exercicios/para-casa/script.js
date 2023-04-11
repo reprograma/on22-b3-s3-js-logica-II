b5706dff4630d484d33329492a3588870052e2a4
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-04-11 15:48:19 -0300

    Resolvendo exercicio4

--- exercicios/para-casa/script.js
+++ exercicios/para-casa/script.js
@@ -1,6 +1,6 @@
 //1. Crie uma função que receba um número como parâmetro e retorne "par" se o número for par e "ímpar" se o número for ímpar.//
 
-function numeroParEImpar(numero){
+function numeroParEImpar(numero) {
     if (numero%2 == 0) {
         console.log(`${numero} é PAR`)
     } else {
