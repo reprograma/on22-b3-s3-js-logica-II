ca75c2ff8536b5fe59eee829162d0141531802f2
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-04-12 08:21:16 -0300

    home exerc_2 correction

--- material/exercicio_para_casa/exercicio_2.js
+++ material/exercicio_para_casa/exercicio_2.js
@@ -9,47 +9,49 @@ function monthNamer (monthNumber) {
     let month
 
     if (monthNumber < 1 || monthNumber > 12 || Number.isInteger(monthNumber) == false) {
-        console.log ("Please insert a valid month number!")
+      month = "Please insert a valid month!"
+      return month
     } else {
         switch (monthNumber) {
             case 1:
                 month = "January"
-                return month
+                break
             case 2:
                 month = "February"
-                return month
+                break
             case 3:
                 month = "March"
-                return month
+                break
             case 4:
                 month = "April"
-                return month
+                break
             case 5:
                 month = "May"
-                return month
+                break
             case 6:
                 month = "June"
-                return month
+                break
             case 7:
                 month = "July"
-                return month
+                break
             case 8:
                 month = "August"
-                return month
+                break
             case 9:
                 month = "September"
-                return month
+                break
             case 10:
                 month = "October"
-                return month
+                break
             case 11:
                 month = "November"
-                return month
+                break
             case 12:
                 month = "December"
-                return month
+                break
             case 12:
         }
+      return month    
     }
 }
 
